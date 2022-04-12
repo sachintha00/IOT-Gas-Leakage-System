72657f271078a0a44512923102ca48f77549a57e
sachintha00 <sachintha0603@gmail.com>
2022-04-12 11:36:03 +0530

    updated

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      sachintha
+   <footer className="ui-footer ui-grid-col-2">
+      <a href="#" className="ui-btn" id="minus">-10%</a>
+      <a href="#" className="ui-btn" id="plus">+10%</a>
+   </footer>
+   <progress className="ui-circle-progress" id="circleprogress" max="100" value="50"></progress>
     </div>
   );
 }
