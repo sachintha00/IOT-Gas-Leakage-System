b59b9408ecf716181d4088c61b66307c8c88ba8a
sachintha00 <sachintha0603@gmail.com>
2022-04-12 07:51:23 +0530

    create nodeMcu code file

---
+++ nodeMcu_Code/nodeMcu_Code.ino
@@ -0,0 +1,19 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+#define FIREBASE_HOST "iotgasleak-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "2as8yvzXXCNksphHOZEwpU0DCC64Gyu2TgpxXRzC"
+
+#define WIFI_SSID "Sachintha"
+#define WIFI_PASSWORD "sachintha"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
